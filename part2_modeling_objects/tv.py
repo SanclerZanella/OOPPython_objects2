e8098861a778ca0031afc93466e4f341f0849abe
Sancler Zanela Jorge Junior <sanclerzjj@gmail.com>
2022-11-03 20:39:43 +0000

    Added definition to concurrency and parallelism and simple thread

--- part2_modeling_objects/tv.py
+++ part2_modeling_objects/tv.py
@@ -120,3 +120,31 @@ oTV.showInfo()
 oTV.setChannel(11)
 oTV.mute()
 oTV.showInfo()
+
+
+# Multiple Instances
+oTV1 = TV()  # create one TV object
+oTV2 = TV()  # create another TV object
+
+# Turn both TVs on
+oTV1.power()
+oTV2.power()
+
+# Raise the volume of TV1
+oTV1.volumeUp()
+oTV1.volumeUp()
+
+# Raise the volume of TV2
+oTV2.volumeUp()
+oTV2.volumeUp()
+oTV2.volumeUp()
+oTV2.volumeUp()
+oTV2.volumeUp()
+
+# Change TV2's channel then mute it
+oTV2.setChannel(44)
+oTV2.mute()
+
+# Now display both TVs
+oTV1.showInfo()
+oTV2.showInfo()
